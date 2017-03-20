ae33b25dcd9006797897f3e48274cc27d2b87aa5
ekalac1 <elzaa_95@hotmail.com>
2017-03-20 12:43:22 +0100

    Fix

--- app/src/main/java/com/hugsby/shoppingapp/API/PlacesApiClient.java
+++ app/src/main/java/com/hugsby/shoppingapp/API/PlacesApiClient.java
@@ -1,12 +1,14 @@
 package com.hugsby.shoppingapp.API;
 
 
+import com.hugsby.shoppingapp.BuildConfig;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class PlacesApiClient {
     public static final String BASE_PLACES_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/";
-    public static final String PLACES_API_KEY = "AIzaSyB0u0MQoduEUDu11sWmTVlBzrt-X411FCw";
+    public static final String PLACES_API_KEY = BuildConfig.PLACES_API_KEY;
     private static Retrofit retrofit = null;
 
 
